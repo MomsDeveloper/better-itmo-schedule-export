3856c4b3da90be28729875c30de00c15e92e49a7
Sugar_Booba <georgiy.khabner@mail.ru>
2023-10-11 03:52:50 +0300

    add functions to form ics file

--- src/popup/parseSchedule.ts
+++ src/popup/parseSchedule.ts
@@ -54,3 +54,48 @@ export async function fetchSchedule(authToken: string, dateFrom: Date, dateTo: D
     const json = await response.json();
     return json['data'];
 }
+
+
+export function scheduleToIcs(schedule: Day[]): string {
+    let result: string = "";
+
+    result += "BEGIN:VCALENDAR\n";
+    result += "VERSION:2.0\n";
+    result += "PRODID:-//Apple Computer\\, Inc//iCal 1.5//EN\n";
+    result += "CALSCALE:GREGORIAN\n";
+    result += "X-WR-CALNAME:my.itmo\n";
+    result += "X-WR-TIMEZONE:Etc/UTC\n";
+    result += "X-PUBLISHED-TTL:PT10M\n";
+    result += "REFRESH-INTERVAL;VALUE=DURATION:PT10M\n";
+    for (const day of schedule) {
+        for (const lesson of day.lessons) {
+            const [time_start, time_end] = joinDateTime(day, lesson);
+            result += "BEGIN:VEVENT\n";
+            result += `SUMMARY:${lesson.subject}\n`;
+            result += `DTSTART;VALUE=DATE-TIME:${time_start}\n`;
+            result += `DTEND;VALUE=DATE-TIME:${time_end}\n`;
+            result += `DESCRIPTION:${lesson.type} - ${lesson.teacher_name}\n`;
+            result += "END:VEVENT\n";
+        }
+    }
+    result += "END:VCALENDAR";
+
+    return result;
+}
+
+function joinDateTime(day: Day, lesson: Lesson): [string, string] {
+    const date_start = new Date(day.date);
+    const time_parts = lesson.time_start.split(':');
+    date_start.setHours(parseInt(time_parts[0]));
+    date_start.setMinutes(parseInt(time_parts[1]));
+    date_start.setSeconds(0);
+
+
+    const date_end = new Date(day.date);
+    const time_parts_end = lesson.time_end.split(':');
+    date_end.setHours(parseInt(time_parts_end[0]));
+    date_end.setMinutes(parseInt(time_parts_end[1]));
+    date_end.setSeconds(0);
+
+    return [date_start.toISOString(), date_end.toISOString()];
+}
